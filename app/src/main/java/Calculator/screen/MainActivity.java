77e511beeb7fc3f81d2d52d6b7b682d79ea6224e
x9731 <x9731@rambler.ru>
2021-07-23 13:57:04 +0300

    Initial commit

---
+++ app/src/main/java/Calculator/screen/MainActivity.java
@@ -0,0 +1,17 @@
+package Calculator.screen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.Shvedov.myproject.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator);
+    }
+}
